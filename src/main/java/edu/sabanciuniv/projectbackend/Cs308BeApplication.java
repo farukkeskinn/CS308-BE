0de631aca668af6ce4dfc3d0fe45407537a3189b
farukkeskinn <keskinaf@icloud.com>
2025-03-10 23:04:14 +0300

    Project started

---
+++ src/main/java/edu/sabanciuniv/projectbackend/Cs308BeApplication.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.projectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs308BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs308BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/sabanciuniv/projectbackend/Cs308BeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.projectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs308BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
